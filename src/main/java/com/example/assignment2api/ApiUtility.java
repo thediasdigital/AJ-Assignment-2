129dc0cac5f60cea68992f9e2d312a8aba8cd947
Bennett Dias <bennett.dias3@gmail.com>
2024-11-13 20:47:46 -0500

    Added ApiUtility class as well as card search view / controller.

---
+++ src/main/java/com/example/assignment2api/ApiUtility.java
@@ -0,0 +1,9 @@
+package com.example.assignment2api;
+
+import com.google.gson.Gson;
+
+public class ApiUtility {
+
+
+
+}
---
+++ src/main/java/com/example/assignment2api/SearchCardViewController.java
@@ -0,0 +1,22 @@
+package com.example.assignment2api;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class SearchCardViewController {
+
+    @FXML
+    private Button getDetailsBtn;
+
+    @FXML
+    private ListView<?> resultsListView;
+
+    @FXML
+    private Button searchBtn;
+
+    @FXML
+    private TextField searchTxtField;
+
+}
